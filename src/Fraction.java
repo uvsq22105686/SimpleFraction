c9739a40b42fda185c4ce8ea8abac01836cff38a
uvsq22105686 <melissa.allaoua@ens.uvsq.fr>
2021-10-01 12:23:25 +0200

    valider les changement

--- src/Fraction.java
+++ src/Fraction.java
@@ -1,3 +1,14 @@
 public class Fraction {
+    private int denumerateur;
+    private int numerateur;
+    public Fraction(int denumerateur,int numerateur) {
+        this.denumerateur=denumerateur;
+        this.numerateur=numerateur;
 
+    }
+    public String toString()
+    {
+        String str = this.numerateur + "et " + this.denumerateur;
+        return str;
+    }
 }
