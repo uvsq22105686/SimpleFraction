143ca3966707d27541fab161ff6942992ade506b
uvsq22105686 <melissa.allaoua@ens.uvsq.fr>
2021-10-01 16:23:13 +0200

    modification

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hello toto");
         Fraction f=new Fraction(15, 10);
         System.out.println(f.toString());
     }
